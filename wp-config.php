9b048c554b5ac0c496a7451312ffc9e507baa74e
Richard Howard <richardahoward@gmail.com>
2012-09-13 10:27:56 -0700

    wordpress caching enabling

--- wp-config.php
+++ wp-config.php
@@ -25,6 +25,8 @@ $table_prefix  = 'wp_';
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
+define('WP_CACHE', true);
+
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
 
